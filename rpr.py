a911895445574e29fee965996b71a49ffe7b2d0f
jonathantsang <j26tsang@gmail.com>
2017-01-07 18:33:55 -0500

    code

---
+++ rpr.py
@@ -0,0 +1,10 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+
+web_page = urlopen("http://reddit.com")
+soup = BeautifulSoup(web_page, 'html.parser')
+l = []
+mydivs = soup.findAll("a", { "class" : "title" })
+for div in mydivs:
+    print(div.text)
